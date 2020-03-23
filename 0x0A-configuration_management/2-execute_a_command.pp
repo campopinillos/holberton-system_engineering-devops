992f11ee611a48f75942d58f12cc49fcf21196c8
campopinillos <1207@holbertonschool.com>
2020-03-23 16:56:52 -0500

    [Fix] Command and path 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
-exec { 'pkill -f killmenow':
+exec { 'pkill':
   command => 'pkill -f killmenow',
   path => '/usr/bin/:/usr/local/bin/:/bin/',
-  returns => [0,1],
 }
