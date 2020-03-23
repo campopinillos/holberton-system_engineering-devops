e52ee3730a451d7d4229411a6e8b901fdf9fe61e
campopinillos <1207@holbertonschool.com>
2020-03-23 17:58:22 -0500

    [Fix] provider

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow
 exec { 'pkill':
-  command => 'pkill -x killmenow',
+  provider => shell,
+  command => 'pkill -f killmenow',
   path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
