f23193eab94743db7e2eb87660267e176658255b
campopinillos <1207@holbertonschool.com>
2020-03-24 21:56:37 -0500

    [Fix] Command 6

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow
-exec { 'pkill':
+exec { 'pkill -x killmenow':
   command => 'pkill -x killmenow',
   path    => '/usr/bin/',
 }
