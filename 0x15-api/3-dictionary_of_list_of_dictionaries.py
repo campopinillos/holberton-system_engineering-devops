9ced68cb2898c28a17c466876d32d7b2c8a3737e
campopinillos <1207@holbertonschool.com>
2020-05-05 12:20:56 -0500

    [Feature] Mandatory task files

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,21 @@
+#!/employee/bin/python3
+"""Python script that using a REST API, for a given employee ID,
+returns information about list progress"""
+
+if __name__ == '__main__':
+    import json
+    import requests
+    url = 'https://jsonplaceholder.typicode.com/'
+    u_id = requests.get(url + "users").json()
+
+    dic = {}
+    for user in u_id:
+        dic[user.get("id")] = [{
+            "username": user.get("username"),
+            "task": task.get("title"),
+            "completed": task.get("completed")
+        } for task in requests.get(url + "todos",
+                                   params={"userId": user.get("id")}).json()]
+
+    with open("todo_all_employees.json", "w", newline='') as f:
+        json.dump(dic, f)
