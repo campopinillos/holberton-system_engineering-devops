a6df85196093e6258107210a5251920b006efa62
campopinillos <1207@holbertonschool.com>
2020-05-07 14:49:01 -0500

    [Fix] Pep8 style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,11 +4,12 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given subreddit"""
+    """prints the titles of the first 10 hot posts listed for a subreddit"""
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0', }
     top = {'limit': 10}
-    response = requests.get(url, headers=headers, params=top, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=top,
+                            allow_redirects=False)
     if response.status_code != 200:
         print('None')
         return
