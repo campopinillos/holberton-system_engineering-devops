667725e482226a4acccebc5d513dd986c99140d0
campopinillos <1207@holbertonschool.com>
2020-05-07 14:19:37 -0500

    [Fix] Style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,14 @@
 """Python script that queries the Reddit API"""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        'User-Agent': 'app/1.0'
-    }
+    headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
     results = response.json().get('data')
     return results.get('subscribers')
-    
